47ccd94d2e7391d8d5f259b40ad57130c1768cbc
amitntrivedi <amitntrivedi@gmail.com>
2020-07-11 10:01:09 +0530

    Router, Dashboard and Login Added

---
+++ src/containers/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import  { Route } from 'react-router-dom'; 
+
+const Dashboard=()=>{
+    return(
+        <div>
+        <div > Dashboard Page</div>
+        <Route path='/' ></Route>
+        </div>
+    ) 
+
+}; 
+
+export default Dashboard; 
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
  import './App.css';
+import Login from '../containers/login/Loing';
+import Dashboard from '../containers/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-         
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-  
-      </header>
+      <Router >
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </Router>
     </div>
   );
 }
-
 export default App;
